be473defbe705fdc56f066aa8654880867be2d43
Andrea Di paola <andr3a.dipaola@gmail.com>
2023-04-06 20:31:55 +0200

    added some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import './App.css';
-import ButtonComponent from './components/ButtonComponent';
-import ImageComponent from './components/ImageComponent';
+import "./App.css";
+import ButtonComponent from "./components/ButtonComponent";
+import ImageComponent from "./components/ImageComponent";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ImageComponent imgSrc="https://images.pexels.com/photos/1804169/pexels-photo-1804169.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" imgAlt="imamgine generica"/>
-        <ButtonComponent buttonText="Click Me!"/>
+        <ImageComponent
+          imgSrc="https://images.pexels.com/photos/1804169/pexels-photo-1804169.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          imgAlt="vintage camera"
+        />
+        <ButtonComponent buttonText="Click Me!" />
       </header>
     </div>
   );
